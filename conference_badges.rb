5b266425a7fb8d615314d227eae14e1b6a845483
Ron Sala <ron.sala@gmail.com>
2017-12-12 05:37:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def batch_badge_creator(name_array)
   name_array.map { |name| "Hello, my name is #{name}." }
 end
 
-def assign_rooms(speakers)
-  room_assignments = []
-  speakers.each_with_index { |speaker| room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
-end
+# def assign_rooms(speakers)
+#   room_assignments = []
+#   speakers.each_with_index { |speaker| room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+# end
